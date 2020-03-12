8211be7c408992430cd7aa250c9d3693739b4279
Robert <rob.kedzior@hotmail.com>
2020-03-12 14:52:47 +0100

    route change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
 import AdminScreen from "./components/AdminScreen";
 import PublicScreen from "./components/PublicScreen";
 import AuthProvider from "./components/Auth";
@@ -9,11 +9,11 @@ import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
     <AuthProvider>
-      <Router>
+      <Switch>
         <Route exact path="/" component={PublicScreen} />
         <Route exact path={"/login"} component={Login} />
         <PrivateRoute exact path="/admin" component={AdminScreen} />
-      </Router>
+      </Switch>
     </AuthProvider>
   );
 }
