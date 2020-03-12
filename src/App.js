f95cb2e805f7a37593b091f6cbb04c8dee8776b2
Robert <rob.kedzior@hotmail.com>
2020-03-12 16:17:11 +0100

    final route commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AdminScreen from "./components/AdminScreen";
 import PublicScreen from "./components/PublicScreen";
 import AuthProvider from "./components/Auth";
