e66ac956b6dffdbf2759f6decdd5e219e51adc4d
Robert <rob.kedzior@hotmail.com>
2020-03-12 15:17:23 +0100

    use browerrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AdminScreen from "./components/AdminScreen";
 import PublicScreen from "./components/PublicScreen";
 import AuthProvider from "./components/Auth";
