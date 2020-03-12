bdaa8106013c92d674a16197ea0e5bf942d92eb7
Robert <rob.kedzior@hotmail.com>
2020-03-12 16:54:11 +0100

    image preview

--- src/components/AdminScreen.js
+++ src/components/AdminScreen.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Camera from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
 import { auth } from "../firebase";
 
 export default function AdminScreen() {
+  const [imageUri, setImageUri] = useState(null);
+
   function handleTakePhoto(dataUri) {
     // Do stuff with the photo...
-    console.log("takePhoto", dataUri);
+    setImageUri(dataUri);
   }
+
+  const handleTakeAnotherPhoto = () => {
+    setImageUri(null);
+  };
 
   return (
     <div>
       <h1>AdminScreen</h1>
       <button onClick={() => auth.signOut()}>Sign Out</button>
       <div style={{ width: "400px;", height: "400px;" }}>
-        <Camera onTakePhotoAnimationDone={handleTakePhoto} />
+        {imageUri ? (
+          <div>
+            <img src={imageUri} alt="userImage" />
+            <button>Use Photo</button>
+            <button onClick={handleTakeAnotherPhoto}>Take another photo</button>
+          </div>
+        ) : (
+          <Camera onTakePhotoAnimationDone={handleTakePhoto} />
+        )}
       </div>
     </div>
   );
