0e63195b348447aab52974b8a0a1bb7bb33d3d07
Robert <rob.kedzior@hotmail.com>
2020-03-16 12:37:08 +0100

    changes

--- src/components/Admin/Personalization.js
+++ src/components/Admin/Personalization.js
@@ -1,49 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
+import { Typography, CssBaseline } from "@material-ui/core";
+import Checkbox from "@material-ui/core/Checkbox";
 import Next from "../Next";
 
 const useStyles = makeStyles(theme => ({
-  container: {
-    backgroundColor: "skyblue",
-    height: "100%"
+  root: {
+    flexGrow: 1,
+    height: "90vh"
   },
-  option: {
-    border: "1px solid black",
-    backgroundColor: theme.palette.secondary.main
+  paper: {
+    cursor: "pointer",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px gray",
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.primary,
+    height: "50%",
+    "&:hover": {
+      color: "white",
+      backgroundColor: theme.palette.text.secondary
+    },
+    "&.active": {
+      color: "white",
+      backgroundColor: theme.palette.text.secondary
+    }
+  },
+  image: {
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "dark"
+        ? theme.palette.grey[900]
+        : theme.palette.grey[50],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    margin: "5px",
+    "&:hover": {
+      opacity: "0.65"
+    }
   }
 }));
 
-export default function Personalization() {
+export default function Personalization({ onPatientClick }) {
   const classes = useStyles();
+  const [selectedPatientId, setSelectedPatientId] = useState(null);
+
+  const handlePatientClick = event => {
+    setSelectedPatientId(event.target.id);
+  };
 
   return (
-    <Container className={classes.container} maxWidth={"false"}>
-      <Grid container spacing={3}>
-        <Typography>Personalization</Typography>
-        <Paper elevation={3}>
-          <Grid item lg={6} className={classes.option}>
-            Option One
-          </Grid>
-        </Paper>
-        <Paper elevation={3}>
-          <Grid item lg={6} className={classes.option}>
-            Option Two
-          </Grid>
-        </Paper>
-        <Paper elevation={3}>
-          <Grid item lg={6} className={classes.option}>
-            Option Three
-          </Grid>
-        </Paper>
-        <Grid item lg={6} className={classes.option}>
-          Option Four
+    <div>
+      <Typography>Personalization Grid</Typography>
+      <CssBaseline />
+      <Grid container component="main" className={classes.root}>
+        <Grid
+          item
+          xs={false}
+          id={1}
+          xl={5}
+          onClick={e => handlePatientClick(e)}
+          className={`${classes.image}`}
+        >
+          <Checkbox
+            checked={parseInt(selectedPatientId) === 1}
+            value="primary"
+            inputProps={{ "aria-label": "primary checkbox" }}
+          />
         </Grid>
+        <Grid
+          item
+          xs={false}
+          id={2}
+          xl={5}
+          className={classes.image}
+          onClick={e => handlePatientClick(e)}
+        >
+          <Checkbox
+            checked={parseInt(selectedPatientId) === 2}
+            value="primary"
+            inputProps={{ "aria-label": "primary checkbox" }}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={false}
+          id={3}
+          xl={5}
+          className={classes.image}
+          onClick={e => handlePatientClick(e)}
+        >
+          <Checkbox
+            checked={parseInt(selectedPatientId) === 3}
+            value="primary"
+            inputProps={{ "aria-label": "primary checkbox" }}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={false}
+          id={4}
+          xl={5}
+          className={classes.image}
+          onClick={e => handlePatientClick(e)}
+        >
+          <Checkbox
+            checked={parseInt(selectedPatientId) === 4}
+            value="primary"
+            inputProps={{ "aria-label": "primary checkbox" }}
+          />
+        </Grid>
+        <Next />
       </Grid>
-      <Next />
-    </Container>
+    </div>
   );
 }
--- src/components/AdminScreen.js
+++ src/components/AdminScreen.js
@@ -1,39 +1,100 @@
 import React, { useState, useEffect } from "react";
 import Camera, { IMAGE_TYPES } from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
+import Button from "@material-ui/core/Button";
 import Personalization from "./Admin/Personalization";
 import PatientsGrid from "./Admin/PatientsGrid";
 import Signout from "./Signout";
 import AdBuilding from "./Admin/AdBuilding";
 import { filterImage } from "../imageTransformer";
+import personalImage from "../assets/images/photo_2020-03-14_10-52-10.jpg";
+import Next from "./Next";
 
 export default function AdminScreen() {
   const [imageUri, setImageUri] = useState(null);
   const [templateImage, setTemplateImage] = useState(null);
+  const [currentStep, setCurrentStep] = useState(0);
+  const [selectedPatient, setSelectedPatient] = useState(null);
+  const [selectedPersonalization, setSelectedPersonalization] = useState(null);
 
   const [adCreationSteps, setAdCreationSteps] = useState({
     stepOne: true,
     stepTwo: false,
     stepThree: false,
-    stepFour: false
+    stepFour: false,
+    stepFive: false
   });
 
   function handleTakePhoto(dataUri) {
     // Do stuff with the photo...
-
     setTemplateImage(dataUri);
     const filteredImageUri = filterImage();
     setImageUri(filteredImageUri);
   }
 
-  const handleUsePhoto = () => {
+  useEffect(() => {
+    setTemplateImage(personalImage);
+    const filteredImageUri = filterImage();
+    setImageUri(filteredImageUri);
+  }, []);
+
+  const moveToNextStep = () => {
     const stepState = { ...adCreationSteps };
+    switch (currentStep) {
+      case 0: {
+        stepState.stepOne = false;
+        stepState.stepTwo = true;
+        setAdCreationSteps(stepState);
+        setCurrentStep(1);
+        break;
+      }
 
-    stepState.stepOne = false;
-    stepState.stepTwo = true;
-
-    // setAdCreationSteps(stepState);
+      case 1: {
+        stepState.stepTwo = false;
+        stepState.stepThree = true;
+        setAdCreationSteps(stepState);
+        setCurrentStep(2);
+        break;
+      }
+      case 2: {
+        stepState.stepThree = false;
+        stepState.stepFour = true;
+        setAdCreationSteps(stepState);
+        setCurrentStep(3);
+        break;
+      }
+      case 3: {
+        stepState.stepFour = false;
+        stepState.stepFive = true;
+        setAdCreationSteps(stepState);
+        setCurrentStep(4);
+        break;
+      }
+      default: {
+        return;
+      }
+    }
   };
+
+  const handleStartAgainClick = () => {
+    const stepState = { ...adCreationSteps };
+    stepState.stepOne = true;
+    stepState.stepTwo = false;
+    stepState.stepThree = false;
+    stepState.stepFour = false;
+    stepState.stepFive = false;
+    setAdCreationSteps(stepState);
+    setCurrentStep(0);
+  };
+
+  // const handleUsePhoto = () => {
+  //   const stepState = { ...adCreationSteps };
+
+  //   stepState.stepOne = false;
+  //   stepState.stepTwo = true;
+
+  //   setAdCreationSteps(stepState);
+  // };
 
   const handleTakeAnotherPhoto = () => {
     setImageUri(null);
@@ -52,10 +113,10 @@ export default function AdminScreen() {
                 style={{ width: "100%", height: "100vh" }}
                 alt="userImage"
               />
-              <button onClick={handleUsePhoto}>Use Photo</button>
-              <button onClick={handleTakeAnotherPhoto}>
+              {/* <Button onClick={handleUsePhoto}>Use Photo</Button> */}
+              <Button onClick={handleTakeAnotherPhoto}>
                 Take another photo
-              </button>
+              </Button>
             </div>
           ) : (
             <Camera
@@ -88,8 +149,9 @@ export default function AdminScreen() {
   return (
     <div style={{ height: "100vh", backgroundColor: "lightblue" }}>
       <Signout />
-      <p>Test Message</p>
+      <Button onClick={handleStartAgainClick}>Start Again</Button>
       {stepDisplay()}
+      <Next onButtonClick={moveToNextStep} />
     </div>
   );
 }
