35eb800c1579f2776ffc0b6d9e1a0c2d631b1df9
Robert <rob.kedzior@hotmail.com>
2020-03-26 14:46:25 +0100

    removed unused import

--- src/components/Admin/AdBuilding.js
+++ src/components/Admin/AdBuilding.js
@@ -2,7 +2,6 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import testImage from "../../assets/images/pngguru.com.png";
 
 const useStyles = makeStyles(theme => ({
   root: {
